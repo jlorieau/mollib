17be7c84f9fd4ed1a739cbe705e45e2ebfabda33
Justin Lorieau <justin@lorieau.com>
2016-08-01 10:46:43 -0500

    Added source file header

--- mollib_report.py
+++ mollib_report.py
@@ -1,6 +1,14 @@
 """
-Creates reports for MolLib.
+Report Renderer for MolLib
+
+   @Author:             Justin L Lorieau <jlorieau>
+   @Date:               2016-07-31T12:32:10-05:00
+   @Last modified by:   jlorieau
+   @Last modified time: 2016-08-01T09:28:59-05:00
+   @License:            Copyright 2016
 """
+
+
 from Report import ReportRenderer, SectionRenderer
 
 class MoleculeReportRenderer(ReportRenderer):
