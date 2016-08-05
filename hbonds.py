b7d2b5e06b22500696ba93c89e50d2117a91c35a
Justin Lorieau <justin@lorieau.com>
2016-08-05 17:58:12 -0500

    Added functions to find aliphatic hydrogen bonds

--- hbonds.py
+++ hbonds.py
@@ -4,7 +4,7 @@ MolLib functions for calculating hydrogen bonds and hydrogen positions.
    @Author:             Justin L Lorieau <jlorieau>
    @Date:               2016-08-03T12:01:01-05:00
    @Last modified by:   jlorieau
-   @Last modified time: 2016-08-05T12:48:52-05:00
+   @Last modified time: 2016-08-05T15:37:44-05:00
    @License:            Copyright 2016
 
 TODO: add hydrogenation functions for HA, HB, and so on
@@ -39,6 +39,8 @@ class HydrogenBond(object):
     minor_classification = ''
     distance = None
     angle = None
+    # TODO make distsance and angle a dict so that multiple distances can be
+    # measured
 
     def __init__(self, **kwargs):
         for k, v in kwargs.items():
@@ -175,54 +177,70 @@ def add_one_sp3_h(molecule, atom_name, target_atom, atom_1, atom_2, atom_3,
 
 
 def find_amide_hbond_partners(molecule):
-    """Finds the hydrogen bond partners between CO and N based on distance.
+    """Finds the backbone amide hydrogen bonds."""
+    hbonds = find_hbond_partners(molecule=molecule,
+                                 donor_name_1='O', donor_name_2='C',
+                                 acceptor_name='HN')
+    classify_amide_hbonds(hbonds)
+    return hbonds
+
+
+def find_aliphatic_hbond_partners(molecule):
+    """Finds the backbone aliphatic hydrogen bonds."""
+    hbonds = find_hbond_partners(molecule=molecule,
+                                 donor_name_1='O', donor_name_2='C',
+                                 acceptor_name='HA')
+    return hbonds
+
+
+def find_hbond_partners(molecule, donor_name_1, donor_name_2, acceptor_name):
+    """Finds the hydrogen bond partners between donor atoms (C, O) and an
+    acceptor (HN) based on distance.
 
     [Required]
-    :molecule:        The MolLib molecule object. This function expects the
-                      amide protons ('HN') to be correctly placed in
-                      the molecule.
-    :donor_atom_1_name:    The first donor atom object for the hbond. ex: 'O'
-    :donor_atom_2_name:    The second donor atom object for the hbond. ex: 'C'
-    :acceptor_atom_1_name: The first acceptor atom object for the hbond.
-                           ex: 'HN'
+    :molecule:      The MolLib molecule object. This function expects the
+                    amide protons ('HN') to be correctly placed in
+                    the molecule.
+    :donor_name_1:  The first donor atom object for the hbond. ex: 'O'
+    :donor_name_2:  The second donor atom object for the hbond. ex: 'C'
+    :acceptor_name: The first acceptor atom object for the hbond. ex: 'HN'
 
     [Returns]
     :possible_hbonds:  An annotated list of possible hydrogen bonds.
-
-    ['type classification', atom_oxygen_i, atom_hydrogen_j, oh_distance,
-    coh_angle]
     """
     # Iterate over carbonyls oxygens
     possible_hbonds = []
     for res_i in molecule.residues:
-        o_i = res_i.get('O', None)
-        c_i = res_i.get('C', None)
-        if o_i is None or c_i is None:  # Skip if carbonyl not found
+        donor_1 = res_i.get(donor_name_1, None)  # typically O
+        donor_2 = res_i.get(donor_name_2, None)  # typically C
+        if donor_1 is None or donor_2 is None:   # Skip if carbonyl not found
             continue
 
         # Construct the normalized vector for the c-o bond
-        co_i = calc_vector(c_i, o_i)
+        vec_i = calc_vector(donor_2, donor_1)
 
         # Find the nearest amide proton
         for res_j in molecule.residues:
-            # Skip if hydrogen not found or if residue 'i' and 'j' are within
-            # 1 residue from each other.
-            h_j = res_j.get('HN', None)
-            if h_j is None or abs(o_i.residue.number - h_j.residue.number) < 2:
+            # Skip if acceptor (hydrogen) not found or if residue 'i' and 'j'
+            # are within 1 residue from each other.
+            acceptor_j = res_j.get(acceptor_name, None)
+            if (acceptor_j is None or
+               abs(donor_1.residue.number - acceptor_j.residue.number) < 2):
                 continue
 
-            # Calculate the HN--O distance. If these aren't within
-            # hbond_cutoff then they're not hydrogen bonded
-            ho_ij = calc_vector(h_j, o_i, normalize=False)
-            r_ij = vector_length(ho_ij)
+            # Calculate the donor-acceptor (HN--O) distance. If these
+            # aren't within hbond_cutoff then they're not hydrogen bonded
+            vec_ij = calc_vector(acceptor_j, donor_1, normalize=False)
+            r_ij = vector_length(vec_ij)
             if r_ij > settings.hbond_cutoff:
                 continue
 
-            # Calculate the C-O--HN angle. Vectors have to be normalized;
-            # co_i is already normalized, but ho_ij isn't yet.
-            ho_ij /= r_ij
+            # Calculate the donor-acceptor (C-O--HN) angle. Vectors have
+            # to be normalized; vec_i is already normalized, but vec_ij
+            # isn't yet.
+            vec_ij /= r_ij
 
-            dot_product = np.dot(ho_ij, co_i)
+            dot_product = np.dot(vec_ij, vec_i)
             theta = acos(dot_product) * 180. / pi
 
             # Add it to the list of possible hydrogen bonds
@@ -232,12 +250,10 @@ def find_amide_hbond_partners(molecule):
             minor_classification = ''
             distance = None
             angle = None
-            hbond = HydrogenBond(donor=o_i, acceptor=h_j, distance=r_ij,
-                                 angle=theta)
+            hbond = HydrogenBond(donor=donor_1, acceptor=acceptor_j,
+                                 distance=r_ij, angle=theta)
             possible_hbonds.append(hbond)
-
-    hbonds = classify_amide_hbonds(possible_hbonds)
-    return hbonds
+    return possible_hbonds
 
 
 def classify_amide_beta_turns(hbond):
@@ -337,8 +353,10 @@ def classify_amide_hbonds(possible_hbonds):
     return possible_hbonds
 
 if __name__ == "__main__":
-    mol = Molecule('2MJB')
+    mol = Molecule('2N7J')
     add_h(mol)
-    mol.write_pdb('2MJB_H.pdb')
+#    mol.write_pdb('2MJB_H.pdb')
     hbonds = find_amide_hbond_partners(mol)
+    pprint(hbonds)
+    hbonds = find_aliphatic_hbond_partners(mol)
     pprint(hbonds)
