734c7e328875ac172d4dfe9f74e6c243e883000f
Justin Lorieau <justin@lorieau.com>
2016-08-17 19:41:34 -0500

    Updated pK settings

--- core/settings.py
+++ core/settings.py
@@ -18,15 +18,20 @@ helix (in degrees)"""
 default_pH = 7.0
 """The default pH of new molecules."""
 
-pKs = {'ASP': 3.5,
-       'GLU': 4.2,
-       'HIS': 6.6,
-       'CYS': 6.8,
-       'TYR': 10.3,
-       'LYS': 10.5,
-       'C-term': 3.3,
-       'N-term': 7.7}
+pKs = {'ASP': {'OD1-OD2': (-1.0, 3.5)},
+       'GLU': {'OE1-OD2': (-1.0, 4.2)},
+       'HIS': {'ND1-NE2': (6.6, 14.0)},
+       'CYS': {'SG': (6.8,)},
+       'TYR': {'OH': (10.3,)},
+       'LYS': {'NZ': (10.5, 14.0, 14.0)},
+       'last': {'O-OXT': (-1.0, 3.3)},
+       'first': {'N': (7.7, 14.0, 14.0),}
+     }
 """The default pKs of ionizable amino-acids. [Ref]_
+
+Some amino-acids have degenerate ionizeable atoms; these are listed and
+separated by '-' characters. The different pKs for each ionization is
+listed in the subsequent items in the tuple.
 
 
   .. [Ref]: G. R. Grimsley, J. M. Scholtz, C. N. Pace,
