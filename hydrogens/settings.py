e7871cc65de00c4b3e115db46e8a442941ffb2eb
Justin Lorieau <justin@lorieau.com>
2016-08-24 22:12:15 -0500

    Simplified bond length settings

--- hydrogens/settings.py
+++ hydrogens/settings.py
@@ -1,8 +1,6 @@
 
 bond_length = {'N-H': 1.023,    # L. Yao, et al. JACS 130, 16518-20 (2008)
                'C-H': 1.117,    # M. Ottiger, et al. JACS 121, 4690-4695 (1999)
-               'CA-HA': 1.117,  # M. Ottiger, et al. JACS 121, 4690-4695 (1999)
-               'C-H3': 1.106,   # M. Ottiger, et al. JACS 121, 4690-4695 (1999)
                'C-C': 1.517,    # M. Ottiger, et al. JACS 121, 4690-4695 (1999)
                'N-H2': 1.01,    # PK Sawinski, et al. Crys Growth & Design
                                 # 13, 1730 (2013)
