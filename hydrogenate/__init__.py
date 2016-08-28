6c1985ac0ca7cf4746f734924b000567ac92533f
Justin Lorieau <justin@lorieau.com>
2016-08-28 06:38:34 -0500

    Removed hydrogenate submodule and moved functionality to hydrogens submodule

--- hydrogenate/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Add hydrogens to a molecular structure.
-"""
-
-from .add_hydrogens import (add_h, add_one_sp2_h, add_two_sp2_h, add_one_sp3_h,
-                            add_two_sp3_h,)
-from . import settings
