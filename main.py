2642ffbafe9686154950c441fa2db5ad1d93fd66
Justin Lorieau <justin@lorieau.com>
2016-08-29 09:50:11 -0500

    Added molecules

--- main.py
+++ main.py
@@ -16,13 +16,17 @@ from mollib.hbonds import (find_amide_hbond_partners,
 
 def do():
     #logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-    mol = Molecule('2KXA')
+    #mol = Molecule('2MUV')
+    mol = Molecule('2PTN')
+    mol = Molecule('2MJB')
     add_hydrogens(mol)
     #    mol.write_pdb('2MJB_H.pdb')
     #hbonds = find_amide_hbond_partners(mol)
     #hbonds = find_aliphatic_hbond_partners(mol)
     #pprint(hbonds)
-    mol.write_pdb('output/2kxa_h.pdb')
+    #mol.write_pdb('output/2PTN_h.pdb')
+    #mol.write_pdb('output/2MUV_h.pdb')
+    mol.write_pdb('output/2MJB_h.pdb')
 
 if __name__ == "__main__":
     do()
