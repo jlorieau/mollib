67d3e500aa7910b034df4da09f6ad37630ad71bc
Justin Lorieau <justin@lorieau.com>
2016-08-31 17:14:17 -0500

    Updated to include other add_h functions

--- hydrogens/__init__.py
+++ hydrogens/__init__.py
@@ -1,2 +1,3 @@
 from . import settings
-from .hydrogenate import add_hydrogens, add_one_sp2_h
+from .hydrogenate import (add_hydrogens, add_one_sp2_h, add_two_sp2_h,
+                          add_one_sp3_h, add_two_sp3_h, add_three_sp3_h)
