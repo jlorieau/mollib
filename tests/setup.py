f74c3c073b583da0f657abb3c5679e294bd3badd
Justin Lorieau <justin@lorieau.com>
2016-08-31 19:54:33 -0500

    Reorganized directory structure

---
+++ tests/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+import mollib
+
+setup(name='mollib',
+      license='GPL V2',
+      author='Justin L Lorieau',
+      description='A Python package to read molecular structures',
+      packages=['mollib'],
+      platforms='any',
+      test_suite='nose.collector',
+)
