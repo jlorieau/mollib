63c1535f165ce66eb3170736736b0bf48e3bab67
Justin Lorieau <justin@lorieau.com>
2016-09-01 06:47:48 -0500

    Added basic command line parser

--- mollib/__main__.py
+++ mollib/__main__.py
@@ -9,25 +9,42 @@ import mollib
 def main():
     parser = argparse.ArgumentParser(prog='mollib',
                                      description='A molecular processor')
-    parser._optionals.title = 'options'
+    # parser._optionals.title = 'arguments'
 
+    subparsers = parser.add_subparsers(title='commands')
+
+    # Commands and subparsers
+    # Measurement subparser
+    mparser = subparsers.add_parser('measure',
+                                    help='Measure geometries of molecules')
+    mparser.add_argument('-md', '--measure-distance', nargs=2, required=False,
+                        metavar='atom',
+                        help=("Measured distance between atoms.'"
+                              "ex: N31-N C32-CA"))
+
+    hbparser = subparsers.add_parser('hbond',
+                                     help='Find hydrogen-bonds of molecules')
+    hbparser.add_argument('-t', '--type', required=True,
+                          choices=['all', 'amide', 'aliphatic'],
+                          help='Determine the hydrogen bonds.')
+
+    # Standard commands
     parser.add_argument('-v', '--version', action='version',
                         version='%(prog)s {}'.format(mollib.__version__))
-    parser.add_argument('-i', '--in', action='append', nargs='+',
-                        type=str, required=True, metavar='identifier',
-                        help=('(required) The filename(s) or PDB identifier(s)'
+    parser.add_argument('id', action='append', nargs='+',
+                        type=str, metavar='identifier/filename',
+                        help=('The filename(s) or PDB identifier(s)'
                               ' of the structure(s)'))
-    parser.add_argument('-o', '--out', nargs=1,
-                        type=str, required=False, metavar='output_file',
+    parser.add_argument('-o', '--out', nargs=1, required=False,
+                        type=str, metavar='filename',
                         help='The output filename of the structure')
-    parser.add_argument('-c', '--config', nargs=1,
-                        type=str, required=False, metavar='config_file',
-                        help='The configuration file to use')
+    parser.add_argument('-c', '--config', nargs=1, required=False,
+                        type=str, metavar='filename',
+                        help='The configuration filename to use')
+
+    # Processing options
     parser.add_argument('--hydrogenate', action='store_true',
                         help='Strip the hydrogens and re-add hydrogens')
-    parser.add_argument('--hbonds', required=False,
-                        choices=['all', 'amide', 'aliphatic'],
-                        help='Determine the hydrogen bonds.3')
 
     args = parser.parse_args()
 
