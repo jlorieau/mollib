7697867dfa6a596acf29e358123fa64b64ee8f26
Justin Lorieau <justin@lorieau.com>
2016-08-31 21:13:47 -0500

    Added command-line arguments.

--- mollib/__main__.py
+++ mollib/__main__.py
@@ -3,31 +3,27 @@
 # Author: Justin L Lorieau
 # Copyright 2016
 
-from pprint import pprint
-import logging
-import sys
+import argparse
+import mollib
 
-from mollib.core import Molecule, measure_angle
-from mollib.hydrogens import add_hydrogens, add_one_sp2_h
-#from mollib.hydrogenate import add_h
-from mollib.hbonds import (find_amide_hbond_partners,
-                           find_aliphatic_hbond_partners)
+def main():
+    parser = argparse.ArgumentParser(prog='mollib',
+                                     description='A molecular processor')
+    parser.add_argument('--version', action='version',
+                        version='%(prog)s {}'.format(mollib.__version__))
+    parser.add_argument('-i', '--in', action='append', nargs='+',
+                        type=str, required=True, metavar='identifier',
+                        help='The filename or PDB identifier of the structure')
+    parser.add_argument('-o', '--out', nargs=1,
+                        type=str, required=False, metavar='output_file',
+                        help='The output filename of the structure')
+    parser.add_argument('--hydrogenate', action='store_true',
+                        help='Strip the hydrogens and re-add hydrogens')
+    parser.add_argument('--hbonds', required=False,
+                        choices=['all', 'amide', 'aliphatic'],
+                        help='Determine the hydrogen bonds.3')
 
-
-def do():
-    #logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-    mol = Molecule('2MUV')
-    #mol = Molecule('2PTN')
-    #mol = Molecule('2MJB')
-    mol.pH = 6
-    add_hydrogens(mol)
-    #    mol.write_pdb('2MJB_H.pdb')
-    #hbonds = find_amide_hbond_partners(mol)
-    #hbonds = find_aliphatic_hbond_partners(mol)
-    #pprint(hbonds)
-    #mol.write_pdb('output/2PTN_h.pdb')
-    mol.write_pdb('output/2MUV_h.pdb')
-    #mol.write_pdb('output/2MJB_h.pdb')
+    args = parser.parse_args()
 
 if __name__ == "__main__":
-    do()
+    main()
