26a8bef3f47a061d6c40c0111f7515a379b9db78
Justin Lorieau <justin@lorieau.com>
2016-09-02 08:48:01 -0500

    Modified the main function to work with the new plugin framework

--- mollib/__main__.py
+++ mollib/__main__.py
@@ -4,49 +4,41 @@
 # Copyright 2016
 
 import argparse
+
 import mollib
+from mollib.plugins import Plugin
+
 
 def main():
+    # Load the plugins
+    plugins = Plugin.plugin_instances()
+
+    # Load the argument parser and subparsers
     parser = argparse.ArgumentParser(prog='mollib',
                                      description='A molecular processor')
-    # parser._optionals.title = 'arguments'
+    subparsers = parser.add_subparsers(title='commands', dest='command',
+                                       metavar='')
+    parser.add_argument('--version', action='version',
+                        version=('%(prog)s ' + mollib.__version__))
 
-    subparsers = parser.add_subparsers(title='commands')
+    # Process the plugin subparsers
+    for plugin in plugins:
+        plugin.options(subparsers)
 
-    # Commands and subparsers
-    # Measurement subparser
-    mparser = subparsers.add_parser('measure',
-                                    help='Measure geometries of molecules')
-    mparser.add_argument('-md', '--measure-distance', nargs=2, required=False,
-                        metavar='atom',
-                        help=("Measured distance between atoms.'"
-                              "ex: N31-N C32-CA"))
-
-    hbparser = subparsers.add_parser('hbond',
-                                     help='Find hydrogen-bonds of molecules')
-    hbparser.add_argument('-t', '--type', required=True,
-                          choices=['all', 'amide', 'aliphatic'],
-                          help='Determine the hydrogen bonds.')
-
-    # Standard commands
-    parser.add_argument('-v', '--version', action='version',
-                        version='%(prog)s {}'.format(mollib.__version__))
-    parser.add_argument('id', action='append', nargs='+',
-                        type=str, metavar='identifier/filename',
-                        help=('The filename(s) or PDB identifier(s)'
-                              ' of the structure(s)'))
-    parser.add_argument('-o', '--out', nargs=1, required=False,
-                        type=str, metavar='filename',
-                        help='The output filename of the structure')
-    parser.add_argument('-c', '--config', nargs=1, required=False,
-                        type=str, metavar='filename',
-                        help='The configuration filename to use')
-
-    # Processing options
-    parser.add_argument('--hydrogenate', action='store_true',
-                        help='Strip the hydrogens and re-add hydrogens')
-
+    # Parse the commands
     args = parser.parse_args()
+
+    # Prepare and preprocess the structure
+    molecules = [mollib.Molecule(identifier) for identifier in args.i[0]]
+    print(args)
+
+    # Find the relevant plugins to execute
+    active_plugins = [plugin for plugin in plugins if plugin.selected(args)]
+
+    for molecule in molecules:
+        for plugin in active_plugins:
+            plugin.process(molecule)
+
 
 if __name__ == "__main__":
     main()
