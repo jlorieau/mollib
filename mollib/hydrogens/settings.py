5d1175ed5510b79841a1e245d0588b9f63b8b918
Justin Lorieau <justin@lorieau.com>
2016-09-17 07:08:03 -0500

    Reformated the settings.py to better work with Sphinx

--- mollib/hydrogens/settings.py
+++ mollib/hydrogens/settings.py
@@ -1,4 +1,4 @@
-
+#: The default optimal length of standard bonds (in Angstroms) for biomolecules
 bond_length = {'N-H': 1.023,    # L. Yao, et al. JACS 130, 16518-20 (2008)
                'C-H': 1.117,    # M. Ottiger, et al. JACS 121, 4690-4695 (1999)
                'C-C': 1.517,    # M. Ottiger, et al. JACS 121, 4690-4695 (1999)
@@ -9,5 +9,3 @@ bond_length = {'N-H': 1.023,    # L. Yao, et al. JACS 130, 16518-20 (2008)
                'C=O': 1.250,
                'C-O': 1.413,
                }
-"""The default optimal length of standard bonds (in Angstroms) for biomolecules
-"""
--- mollib/utils/settings.py
+++ mollib/utils/settings.py
@@ -1,7 +1,3 @@
-"""
-Utility Settings
-"""
-
+#: The default max width (in characters) of markdown tables (MDTable)
 default_table_max_width = 80
-"""The default max width (in characters) of markdown tables (MDTable)
-"""
+
