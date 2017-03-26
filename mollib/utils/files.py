2d4aa89046467492b4196939cb81cc1237e47538
Justin Lorieau <justin@lorieau.com>
2017-03-26 07:11:32 -0500

    Added file utility functions and refactored to move checks to its own file.

--- mollib/utils/files.py
+++ mollib/utils/files.py
@@ -1,34 +1,6 @@
 """
-Utility functions for checking, reading and writing files.
+Utility functions for reading and writing files.
 """
-
-import logging
-import os
-
-from . import settings
-
-
-def check_file(filename, critical=True):
-    """Check whether the filename exists.
-
-    Parameters
-    ----------
-    filename: str
-        The name of the file to check.
-    critical: bool (optional)
-        If the existence of the file or path is critical to proceed, and the
-        file doesn't exist, end the program execution.
-
-    Returns
-    -------
-    None
-    """
-    if not os.path.isfile(filename):
-        msg = "Filename '{}' does not exist."
-        logging.error(msg.format(filename))
-        if critical:
-            exit()
-    return None
 
 
 def write_txt_file(filename, string, overwrite=None):
