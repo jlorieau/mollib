43f862d1861cfaacf8199d48ef59843be67cd213
Justin Lorieau <justin@lorieau.com>
2017-04-01 10:50:14 -0500

    set default table max width to 120 characters

--- mollib/utils/settings.py
+++ mollib/utils/settings.py
@@ -1,4 +1,7 @@
 #: The default max width (in characters) of markdown tables (MDTable)
 #: This is the value used when outputting tables outside of the terminal
-default_table_max_width = 80
+default_table_max_width = 120
+
+#: When writing files, overwrite them if they already exist.
+overwrite_files = True
 
