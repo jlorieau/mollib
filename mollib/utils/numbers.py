44f80deffcd4efbe9d8b22840cd0b07e17a7388d
Justin Lorieau <justin@lorieau.com>
2017-05-09 06:19:24 -0500

    PEP8 updates

--- mollib/utils/numbers.py
+++ mollib/utils/numbers.py
@@ -3,12 +3,14 @@ Utility functions for dealing with numbers.
 """
 from math import floor, log10
 
+
 def center(number, rjust=4):
     """Center a number string about the decimal."""
     number = str(number)
     split = number.split('.')
     return (''.join((split[0].rjust(4), '.', split[1]))
             if len(split) > 1 else split[0])
+
 
 def round_sig(x, sig=2):
     """Round a number to the specified number of significant figures."""
