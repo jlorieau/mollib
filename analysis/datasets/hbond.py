89f8ab09004b6fb53f39fb4279c04d7cb15422d5
Justin Lorieau <justin@lorieau.com>
2017-06-01 23:20:32 -0500

    Added plotting of theta/d and phi/d values separately

--- analysis/datasets/hbond.py
+++ analysis/datasets/hbond.py
@@ -30,7 +30,7 @@ for i in ('xtick', 'ytick'):
 
 # Histogram parameters
 d_hist_range = (1.5, 3.0)
-# d_hist_range = (-180, 180)
+phi_hist_range = (-180, 180)
 theta_hist_range = (60, 180.)
 bins = 16
 
@@ -61,8 +61,9 @@ for identifier, return_dict in measurement_dict.items():
             continue
         values = d1a1_theta.setdefault(minor, list())
         for d in hbond_dict_list:
-            # values += [(d['angles']['theta'], d['angles']['phi'])]
-            values += [(d['distances']['d1a1'], d['angles']['theta'])]
+            values += [(d['distances']['d1a1'],
+                        d['angles']['theta'],
+                        d['angles']['phi'])]
 
 
 # Define the plots. These are organized by keys in results and the
@@ -85,6 +86,8 @@ def grouper(n, iterable, fillvalue=None):
     return zip_longest(*args, fillvalue=fillvalue)
 
 
+# Plot the d_a1d1 vs theta plots
+
 subfigure_groups = list(grouper(4, labels))
 
 for row, subfigure_labels in enumerate(subfigure_groups, 1):
@@ -104,11 +107,12 @@ for row, subfigure_labels in enumerate(subfigure_groups, 1):
         title, cmap = values
 
         # Prepare the data
-        xy = d1a1_theta[title]
+        xyz = d1a1_theta[title]
 
-        x = np.array([i[0] for i in xy])
-        y = np.array([i[1] for i in xy])
+        x = np.array([i[0] for i in xyz])  # d_a1d1
+        y = np.array([i[1] for i in xyz])  # theta
 
+        # Make the histogram for d_a1d1 vs theta
         hist2d, y, x = np.histogram2d(y, x, bins=36, range=np.array(
             [theta_hist_range, d_hist_range]))
         hist2d = -1. * np.log(hist2d + 0.1)
@@ -149,7 +153,81 @@ for row, subfigure_labels in enumerate(subfigure_groups, 1):
 
         axarr[count].contourf(x[:-1], y[:-1], hist2d, levels, cmap=cmap)
 
-    plt.savefig('hbond_countour_{}.png'.format(row), format='PNG',
-                dpi=1200, bbox_inches='tight', pad_inches=0.05)
-    plt.savefig('hbond_countour_{}.svg'.format(row), format='SVG',
-                bbox_inches='tight', pad_inches=0.05)
+    plt.savefig('hbonds/hbond_countour_d_vs_theta_{}.png'.format(row),
+                format='PNG', dpi=1200, bbox_inches='tight', pad_inches=0.05)
+    plt.savefig('hbonds/hbond_countour_d_vs_theta_{}.svg'.format(row),
+                format='SVG', bbox_inches='tight', pad_inches=0.05)
+
+# Plot the d_a1d1 vs phi plots
+plt.clf()
+plt.cla()
+
+
+for row, subfigure_labels in enumerate(subfigure_groups, 1):
+
+    # Create the subplots in this row
+    f, axarr = plt.subplots(1, len(subfigure_labels), sharey=True,
+                            figsize=figsize)
+
+    # Set the shared y-axis label
+    axarr[0].set_ylabel(r'$\phi (deg)', fontsize=axes_fontsize,
+                        fontname=axes_font)
+
+    for count, values in enumerate(subfigure_labels):
+        if values is None:
+            axarr[count].axis('off')
+            continue
+        title, cmap = values
+
+        # Prepare the data
+        xyz = d1a1_theta[title]
+
+        x = np.array([i[0] for i in xyz])  # d_a1d1
+        z = np.array([i[2] for i in xyz])  # phi
+
+        # Make the histogram for d_a1d1 vs theta
+        hist2d, z, x = np.histogram2d(z, x, bins=36, range=np.array(
+            [phi_hist_range, d_hist_range]))
+        hist2d = -1. * np.log(hist2d + 0.1)
+
+        minimum = np.min(hist2d)
+        hist2d -= minimum
+
+        # Set the plot title
+        axarr[count].set_title(title.replace('__', ' ')
+                               .replace('alpha', "$\\alpha$")
+                               .replace('pi', "$\\pi$")
+                               .replace('No', 'no'),
+                               size=title_fontsize,
+                               fontname=title_font)
+
+        # Set the x-axis label
+        axarr[count].set_xlabel(r'$d_{H...O}$ (A)', fontsize=axes_fontsize,
+                                fontname=axes_font)
+
+        # Set the x-axis ticks
+        # axarr[count].xaxis.set_ticks(np.arange(1.5, 3.0, 0.5))
+        # axarr[count].set_xlim(1.5, 3.0)
+
+        # Set the axis tick label size
+        axarr[count].tick_params(labelsize=label_fontsize)
+
+        # Set the axis tick label font
+        labels = axarr[count].get_xticklabels() + axarr[count].get_yticklabels()
+        for label in labels:
+            label.set_fontname(axes_font)
+
+        # Place ticks on the bottom and left side only
+        axarr[count].get_xaxis().tick_bottom()
+        axarr[count].get_yaxis().tick_left()
+
+        # Set the contour levels
+        levels = np.arange(0.0, 5.1, 1.0)
+
+        axarr[count].contourf(x[:-1], y[:-1], hist2d, levels, cmap=cmap)
+
+    plt.savefig('hbonds/hbond_countour_d_vs_phi_{}.png'.format(row),
+                format='PNG', dpi=1200, bbox_inches='tight', pad_inches=0.05)
+    plt.savefig('hbonds/hbond_countour_d_vs_phi_{}.svg'.format(row),
+                format='SVG', bbox_inches='tight', pad_inches=0.05)
+
