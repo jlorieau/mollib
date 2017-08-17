fdcc239ab206ba370018fe1abace849a32eb1afe
Justin Lorieau <justin@lorieau.com>
2017-08-17 13:17:15 -0500

    Update description and bump version number

--- setup.py
+++ setup.py
@@ -1,6 +1,33 @@
+"""
+Mollib is Python package to analyze and process molecular structures and data.
+
+Mollib is a *unified* command-line program and Python library for the
+validation, quality analysis and manipulation of molecular structures with an
+emphasis on biophysical analysis. Mollib is built on a plugin framework to
+easily add new tools to manipulate and analyze structures and data, which can
+then be *combined* and *cross-validated*.
+
+Mollib includes tools for:
+
+- The :ref:`processing <process-command>` and protonation of
+  molecules.
+- The analysis of :ref:`hydrogen bonds <hbonds-command>` and hydrogen bond
+  quality compared to the highest-resolution PDB structures. Hydrogen bonds
+  are classified based on their acceptor-donor residues and Ramachandran
+  angles
+- The :ref:`measurement <measure-command>` of geometries within molecules
+  as well as the reporting and classification of Ramachandran angles.
+- The statistical analysis and comparison of structures to high-resolution
+  crystal structures.
+- The analysis of :ref:`partial alignment <pa-command>` data with residual
+  dipolar coupling (RDC) and residual anisotropic chemical shift (RACS, a.k.a
+  RCSA) data.
+"""
+
 from setuptools import setup, find_packages, Extension
 from distutils.command.build_ext import build_ext
 
+DOCLINES = (__doc__ or '').split("\n")
 
 # Setup numpy include directory.
 # 1. The script will not add 'numpy' to the setup_requires list, if it
@@ -96,8 +123,8 @@ setup(name='mollib',
       version=__version__,
       license='GPLv3',
       author='Justin L Lorieau',
-      description=('A Python package to analyze and process molecular '
-                   'structures and data'),
+      description=DOCLINES[0],
+      long_description="\n".join(DOCLINES[2:]),
       include_package_data=True,
       packages=find_packages(),
       platforms='any',
