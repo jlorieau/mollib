7aa4356e1e06532e98fdb932d31403ac113aa92a
Justin Lorieau <justin@lorieau.com>
2017-08-17 14:17:15 -0500

    Removed RST syntax from the description

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 """
 Mollib is Python package to analyze and process molecular structures and data.
 
-Mollib is a *unified* command-line program and Python library for the
+Mollib is a unified command-line program and Python library for the
 validation, quality analysis and manipulation of molecular structures with an
 emphasis on biophysical analysis. Mollib is built on a plugin framework to
 easily add new tools to manipulate and analyze structures and data, which can
@@ -9,19 +9,16 @@ then be *combined* and *cross-validated*.
 
 Mollib includes tools for:
 
-- The :ref:`processing <process-command>` and protonation of
-  molecules.
-- The analysis of :ref:`hydrogen bonds <hbonds-command>` and hydrogen bond
-  quality compared to the highest-resolution PDB structures. Hydrogen bonds
-  are classified based on their acceptor-donor residues and Ramachandran
-  angles
-- The :ref:`measurement <measure-command>` of geometries within molecules
-  as well as the reporting and classification of Ramachandran angles.
+- The processing and protonation of molecules.
+- The analysis of hydrogen bonds and hydrogen bond quality compared to the 
+  highest-resolution PDB structures. Hydrogen bonds are classified based on 
+  their acceptor-donor residues and Ramachandran angles
+- The measurement of geometries within molecules as well as the reporting and 
+  classification of Ramachandran angles.
 - The statistical analysis and comparison of structures to high-resolution
   crystal structures.
-- The analysis of :ref:`partial alignment <pa-command>` data with residual
-  dipolar coupling (RDC) and residual anisotropic chemical shift (RACS, a.k.a
-  RCSA) data.
+- The analysis of partial alignment data with residual dipolar coupling (RDC) 
+  and residual anisotropic chemical shift (RACS, a.k.a RCSA) data.
 """
 
 from setuptools import setup, find_packages, Extension
